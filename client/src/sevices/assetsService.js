514595936685ef12713b7e14e20df1f982a55ce9
MHMighani <mh.mgh2012@gmail.com>
2021-10-23 19:54:09 +0330

    post service added

--- client/src/sevices/assetsService.js
+++ client/src/sevices/assetsService.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 import { serverUrl } from "../config.json";
 
-export function deleteAsset(id) {
-  return axios.delete(id);
-}
-
 export function getAssets() {
   return axios.get(`${serverUrl}/assets`);
+}
+
+export function addAsset(assetData) {
+  return axios.post(`${serverUrl}/assets`, assetData);
+}
+
+export function deleteAsset(id) {
+  return axios.delete(id);
 }
