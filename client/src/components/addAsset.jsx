e4b2bf318f9f120b31634fc3fbb3230669997b88
MHMighani <mh.mgh2012@gmail.com>
2021-10-27 08:01:01 +0330

    feat: add assset

---
+++ client/src/components/addAsset.jsx
@@ -0,0 +1,57 @@
+import { addAsset } from "../sevices/assetsService";
+import { useState } from "react";
+const AddAsset = () => {
+  const [state, setState] = useState({});
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await addAsset(state);
+    console.log(response.status);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setState({ ...state, [name]: value });
+  };
+
+  console.log(state);
+
+  return (
+    <div className="addAsset container">
+      <form className="form-group" onSubmit={handleSubmit}>
+        <label htmlFor="asset-choice">نوع دارایی</label>
+        <select
+          name="id"
+          className="custom-class form-control"
+          onChange={handleChange}
+        >
+          <option value=""></option>
+          <option value="coin">تمام سکه</option>
+          <option value="halfCoin">نیم سکه</option>
+          <option value="quarterCoin">ربع سکه</option>
+        </select>
+        <label htmlFor="spot-price">قیمت روز</label>
+        <input
+          name="price"
+          type="number"
+          className="form-control"
+          onChange={handleChange}
+        />
+        <label htmlFor="amount">مقدار</label>
+        <input
+          name="amount"
+          type="number"
+          className="form-control"
+          onChange={handleChange}
+        />
+        <label htmlFor="date">تاریخ خرید</label>
+        <input name="date" type="date" className="form-control" />
+        <button type="submit" className="btn btn-primary">
+          تایید
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddAsset;
