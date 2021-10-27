ab0ad3038d4d8e3a1567e677f4ab66c545b9fddd
MHMighani <mh.mgh2012@gmail.com>
2021-10-27 08:01:01 +0330

    feat: add assset

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import Assets from "./components/assets";
 import { Route, Switch } from "react-router-dom";
+import Assets from "./components/assets";
+import AddAsset from "./components/addAsset";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/assets" component={Assets} />
+        <Route exact path="/add" component={AddAsset} />
       </Switch>
     </div>
   );
--- client/src/components/assets.jsx
+++ client/src/components/assets.jsx
@@ -1,24 +1,32 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { deleteAsset, getAssets } from "../sevices/assetsService";
+import { getPrices } from "../sevices/pricesService";
 import AssetsTable from "./assetsTable";
 
 const Assets = () => {
   const [assetsData, setAssetsData] = useState([]);
+  const [prices, setPrices] = useState({});
 
   useEffect(() => {
     async function fetchApi() {
-      let response = await getAssets();
-
-      setAssetsData(response.data);
+      let assetsResponse = await getAssets();
+      let pricesResponse = await getPrices();
+      setAssetsData(assetsResponse.data);
+      setPrices(pricesResponse.data);
     }
 
     fetchApi();
   }, []);
 
   return (
-    <div className="App">
+    <div className="assets">
+      <Link to="/add" className="btn btn-primary">
+        اضافه کردن
+      </Link>
       <AssetsTable
         assetsData={assetsData}
+        prices={prices}
         onDeleteAsset={async (id) => {
           const newAssetsData = assetsData.filter((asset) => asset.id !== id);
 
