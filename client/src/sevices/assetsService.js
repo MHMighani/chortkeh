a64e53fdf5d10ae648e70b5d294d28b49bce5879
MHMighani <mh.mgh2012@gmail.com>
2021-11-06 12:27:18 +0330

    assets service updated

--- client/src/sevices/assetsService.js
+++ client/src/sevices/assetsService.js
@@ -1,14 +1,24 @@
 import axios from "axios";
 import { serverUrl } from "../config.json";
 
+const baseUrl = `${serverUrl}/assets`;
+
 export function getAssets() {
-  return axios.get(`${serverUrl}/assets`);
+  return axios.get(`${baseUrl}`);
+}
+
+export function getAsset(id) {
+  return axios.get(`${baseUrl}/${id}`);
+}
+
+export function editAsset(id, editedField) {
+  return axios.patch(`${baseUrl}/${id}`, editedField);
 }
 
 export function addAsset(assetData) {
-  return axios.post(`${serverUrl}/assets`, assetData);
+  return axios.post(`${baseUrl}`, assetData);
 }
 
 export function deleteAsset(id) {
-  return axios.delete(`${serverUrl}/assets/${id}`);
+  return axios.delete(`${baseUrl}/${id}`);
 }
