be0f7f663913db5ee85d6486e0f3df9850c65d52
MHMighani <mh.mgh2012@gmail.com>
2021-12-24 08:39:37 +0330

    stockprice service added

--- client/src/services/pricesServices.js
+++ client/src/services/pricesServices.js
@@ -4,3 +4,10 @@ import { serverUrl } from "../config.json";
 export function getPrices(assetClass) {
   return axios.get(`${serverUrl}/${assetClass}`);
 }
+
+export function getStockPrice(stockId) {
+  const url = `${serverUrl}/stock`;
+  const data = axios.get(`${url}/${stockId}`);
+
+  return data;
+}
