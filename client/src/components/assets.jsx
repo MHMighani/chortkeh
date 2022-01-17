9f89999dbd8d04be198f7d60d26707405807f26a
MHMighani <mh.mgh2012@gmail.com>
2022-01-17 17:51:53 +0330

    cash assets seperate table added

--- client/src/components/assets.jsx
+++ client/src/components/assets.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import _ from "lodash";
 import { Link } from "react-router-dom";
 import { deleteAsset, getAssets } from "../services/assetsServices";
 import { getPrices } from "../services/pricesServices";
 import CustomModal from "./modal";
 import AssetsTable from "./assetsTable";
 import mapPricesToAssets from "../utils/mapPricesToAssets";
+import CashTable from "./cashTable";
 
 const Assets = () => {
   const [assetsData, setAssetsData] = useState([]);
@@ -12,21 +14,59 @@ const Assets = () => {
   const [delMessageDisplay, setDelMessageDisplay] = useState(false);
   const [toDeleteAsset, setToDeleteAsset] = useState(null);
 
+  const tables = {
+    goldcurrency: AssetsTable,
+    goldCurrency: AssetsTable,
+    stock: AssetsTable,
+    cash: CashTable,
+  };
+
+  // getting updated market prices
   useEffect(() => {
     async function fetchApi() {
-      let assetsResponse = await getAssets();
       let goldCurrencyPricesResponse = await getPrices("goldcurrency");
       let stockPricesResponse = await getPrices("stock");
 
-      setAssetsData(assetsResponse.data);
       setPrices({
         goldCurrency: [...goldCurrencyPricesResponse.data],
         stock: [...stockPricesResponse.data],
       });
     }
+    fetchApi();
+  }, []);
+
+  // getting assets information
+  useEffect(() => {
+    async function fetchApi() {
+      let { data } = await getAssets();
+
+      setAssetsData(data);
+    }
 
     fetchApi();
   }, []);
+
+  function renderAssetTables() {
+    const data = _.groupBy(assetsData, "assetClass");
+
+    return Object.values(data).map((assets) => {
+      const assetClass = assets[0].assetClass;
+      const table = tables[assetClass];
+
+      let props;
+
+      if (assetClass === "cash") {
+        props = { assetsData: assets, onDeleteAsset: handleDelMsgDisplay };
+      } else {
+        props = {
+          assetsData: mapPricesToAssets(prices, assets) || [],
+          onDeleteAsset: handleDelMsgDisplay,
+        };
+      }
+
+      return React.createElement(table, props);
+    });
+  }
 
   const handleDelMsgClose = () => {
     setDelMessageDisplay(false);
@@ -57,10 +97,8 @@ const Assets = () => {
       <Link to="add" className="btn btn-primary">
         افزودن
       </Link>
-      <AssetsTable
-        assetsData={mapPricesToAssets(prices, assetsData) || []}
-        onDeleteAsset={handleDelMsgDisplay}
-      />
+
+      {renderAssetTables()}
     </div>
   );
 };
