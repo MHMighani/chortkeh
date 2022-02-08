b91d75310b4a3c315ae82ac390a33a12ec661036
MHMighani <mh.mgh2012@gmail.com>
2022-02-08 18:24:47 +0330

    feat: asset details

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { ToastContainer } from "react-toastify";
 import NavbarT from "./components/navbar";
 import StockForm from "./components/stockForm";
 import CashForm from "./components/cashForm";
+import AssetDetails from "./components/assetDetails";
 
 import "react-toastify/dist/ReactToastify.css";
 
@@ -15,6 +16,7 @@ function App() {
       <NavbarT />
       <Switch>
         <Route exact path="/assets" component={Assets} />
+        <Route path="/assets/details/" component={AssetDetails} />
         <Route exact path="/add/goldcurrency/" component={GoldCurrencyForm} />
         <Route exact path="/add/stock" component={StockForm} />
         <Route exact path="/add/cash" component={CashForm} />
