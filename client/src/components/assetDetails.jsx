6a3f44363d056d78cfd9d59ae7e7f545584283a5
MHMighani <mh.mgh2012@gmail.com>
2022-02-08 18:24:47 +0330

    feat: asset details

---
+++ client/src/components/assetDetails.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import { getAssetsBySubClass } from "../services/assetsServices";
+import { getPriceBySubClass } from "../services/pricesServices";
+import getPercentChange from "../utils/getPercentChange";
+import { Card } from "react-bootstrap";
+import Table from "./table";
+
+const AssetDetails = (props) => {
+  const { assetClass, assetSubClass } = props.location.state;
+  const [assetData, setAssetData] = useState([]);
+  const [marketPrice, setMarketPrice] = useState(0);
+
+  // getting assets
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await getAssetsBySubClass(assetSubClass);
+      setAssetData(data);
+    }
+    fetchData();
+  }, [assetSubClass]);
+
+  // getting market price
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await getPriceBySubClass(assetClass, assetSubClass);
+      const priceKey = assetClass === "stock" ? "lastTradePrice" : "price";
+      setMarketPrice(data[priceKey]);
+    }
+
+    fetchData();
+  }, [assetSubClass, assetClass]);
+
+  function getStringDate(date) {
+    return date.join("/");
+  }
+
+  function getProcessedData(data) {
+    return data.map((item) => {
+      const date = { ...item.purchaseDate };
+
+      const overallValue = item.amount * marketPrice;
+      const changePercent = getPercentChange(item.purchasePrice, marketPrice);
+      const purchaseDate = getStringDate(Object.values(date));
+
+      return { ...item, overallValue, changePercent, purchaseDate };
+    });
+  }
+
+  const columns = [
+    { name: "rowNum", label: "ردیف" },
+    { name: "amount", label: "مقدار" },
+    { name: "purchasePrice", label: "قیمت خرید" },
+    { name: "changePercent", label: "درصد سود یا زیان" },
+    { name: "purchaseDate", label: "تاریخ خرید" },
+    { name: "overallValue", label: "ارزش کل" },
+    // { name: "editBtn" },
+    // { name: "deleteBtn" },
+  ];
+
+  return (
+    <div>
+      <Card>
+        <Card.Header>
+          <span>{assetData.length && assetData[0].label}</span>
+          <span>{marketPrice}</span>
+        </Card.Header>
+        <Card.Body>
+          <Table data={getProcessedData(assetData)} columns={columns} />
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default AssetDetails;
