1a78538b6bc3f69a5a7a54d5abd782d4815972a8
MHMighani <mh.mgh2012@gmail.com>
2022-02-08 08:40:32 +0330

    services added

--- client/src/services/assetsServices.js
+++ client/src/services/assetsServices.js
@@ -19,6 +19,33 @@ export function addAsset(assetData) {
   return axios.post(`${baseUrl}`, assetData);
 }
 
+export function addAssets(assetsArray) {
+  const promises = assetsArray.map((asset) => addAsset(asset));
+
+  return Promise.all(promises);
+}
+
 export function deleteAsset(id) {
   return axios.delete(`${baseUrl}/${id}`);
+}
+
+export function getAssetsBySubClass(subClass) {
+  const url = `${baseUrl}?assetSubClass=${subClass}`;
+  return axios.get(url);
+}
+
+// deletes all assets of specific subClass
+export async function deleteAssetBySubClass(subClass) {
+  const { data: assets } = await getAssetsBySubClass(subClass);
+
+  let promises = assets.map((asset) => deleteAsset(asset.id));
+
+  return Promise.all(promises);
+}
+
+export async function deleteAllAssets() {
+  const { data: allAssets } = await getAssets();
+  let promises = allAssets.map((asset) => deleteAsset(asset.id));
+
+  return Promise.all(promises);
 }
