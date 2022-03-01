493d0dded2bc330190cfa28baa8fef62340f164d
MHMighani <mh.mgh2012@gmail.com>
2022-03-01 12:37:16 +0330

    fix: date format

--- client/src/services/historyService.js
+++ client/src/services/historyService.js
@@ -6,7 +6,14 @@ const baseUrl = `${serverUrl}/history`;
 
 // returns date id by persian format
 function getDateId() {
-  return Object.values(utils("fa").getToday()).join("-");
+  //adding zero to the beggining for consistency in dates
+  const lengthOfNumbers = { year: 4, month: 2, day: 2 };
+  const today = utils("fa").getToday();
+  for (let key in today) {
+    today[key] = String(today[key]).padStart(lengthOfNumbers[key], "0");
+  }
+
+  return Object.values(today).join("-");
 }
 
 export function getHistoryRecord(date = "") {
