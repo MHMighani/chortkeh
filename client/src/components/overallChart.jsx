d1340ebc2f05444595f577fcbdd4bf27aee6be8f
MHMighani <mh.mgh2012@gmail.com>
2022-03-05 19:49:24 +0330

    chart style

--- client/src/components/overallChart.jsx
+++ client/src/components/overallChart.jsx
@@ -8,31 +8,37 @@ import {
   Line,
 } from "recharts";
 
+import getCommaSepNum from "../utils/getCommaSepNum";
+
 const OverallChart = ({ data }) => {
-  console.log(data);
-  const newData = data.map((item) => ({
-    name: item.id,
-    overall: item.overall,
-    goldCurrency: item.goldCurrency,
-  }));
+  const chartData = data.map((item) => {
+    return {
+      name: item.id,
+      overall: item.overall,
+      goldCurrency: item.goldCurrency,
+    };
+  });
 
-  //   const newData = [{ name: "1400-12-09", overall: 1200000000 }];
-
-  console.log(newData);
   return (
     <LineChart
+      style={{ margin: "0 auto" }}
       width={730}
       height={250}
-      data={newData}
-      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      data={chartData}
+      margin={{ top: 5, right: 30, left: 100, bottom: 5 }}
     >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis type="number" domain={["dataMin- 10000000", "dataMax"]} />
-      <Tooltip />
       <Legend />
-      <Line type="monotone" dataKey="overall" stroke="#8884d8" />
-      {/* <Line type="monotone" dataKey="goldCurrency" stroke="#82ca9d" /> */}
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis style={{ fontSize: "14px" }} dataKey="name" />
+      <YAxis
+        style={{ fontSize: "14px" }}
+        tickFormatter={(value) => getCommaSepNum(value)}
+        domain={["dataMin- 10000000", "dataMax + 10000000"]}
+        dataKey="overall"
+      />
+      <Tooltip formatter={(value) => getCommaSepNum(value)} />
+
+      <Line type="monotone" dataKey="overall" name="ارزش کل" stroke="#8884d8" />
     </LineChart>
   );
 };
