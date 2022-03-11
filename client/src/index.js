53e661cf367c933b935ae1f0a109fee3bb4e027e
MHMighani <mh.mgh2012@gmail.com>
2022-03-11 20:22:26 +0330

    add persian font

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.rtl.css";
+import "./fonts/b-nazanin-regular.ttf";
+import "./styles.scss";
 
 ReactDOM.render(
   <React.StrictMode>
