a62bf5704e6f2d2d2c8bbbbd0a96ce98587829ab
MHMighani <mh.mgh2012@gmail.com>
2022-03-11 12:11:09 +0330

    add label for purchase date

--- client/src/components/purchaseDate.jsx
+++ client/src/components/purchaseDate.jsx
@@ -1,16 +1,24 @@
 import DatePicker from "react-modern-calendar-datepicker";
 import "react-modern-calendar-datepicker/lib/DatePicker.css";
 
-const PurchaseDate = ({ value, onChange }) => {
+const PurchaseDate = ({
+  value,
+  onChange,
+  label = "تاریخ خرید",
+  name = "purchaseDate",
+}) => {
   return (
-    <DatePicker
-      value={value}
-      onChange={onChange}
-      inputPlaceholder="تاریخ خرید دارایی را انتخاب کنید"
-      locale="fa"
-      name="purchaseDate"
-      shouldHighlightWeekends
-    />
+    <div className="form-group purchase-date">
+      {label && <label htmlFor={name}>{label}</label>}
+      <DatePicker
+        value={value}
+        onChange={onChange}
+        inputPlaceholder="تاریخ خرید دارایی را انتخاب کنید"
+        locale="fa"
+        name="purchaseDate"
+        shouldHighlightWeekends
+      />
+    </div>
   );
 };
 
