0bda5e0bde1fb79c1c123d24d487e6390d5ef22e
MHMighani <mh.mgh2012@gmail.com>
2022-04-10 15:27:07 +0430

    Quota chart Legend and label added

--- client/src/components/quoteChart.jsx
+++ client/src/components/quoteChart.jsx
@@ -1,39 +1,79 @@
-import { PieChart, Pie, Cell } from "recharts";
+import React from "react";
+import { PieChart, Pie, Cell, Legend } from "recharts";
 
-const QuoteChart = ({ data }) => {
-  const COLORS = {
-    goldCurrency: "gold",
-    cash: "silver",
-    stock: "green",
-  };
-
-  const pieChartData = data.map((item) => ({
-    name: item.assetClass,
-    value: item.overallValue,
-  }));
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
   return (
-    <div className="chartWrapper">
-      <PieChart width={500} height={500}>
-        <Pie
-          data={pieChartData}
-          dataKey="value"
-          nameKey="name"
-          cx="50%"
-          cy={250}
-          width={400}
-          height={400}
-          outerRadius={200}
-          fill="#8884d8"
-          label
-        >
-          {pieChartData.map((entry, index) => (
-            <Cell key={index} fill={COLORS[entry.name]} />
-          ))}
-        </Pie>
-      </PieChart>
-    </div>
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
   );
 };
 
-export default QuoteChart;
+class QuotaChart extends React.PureComponent {
+  render() {
+    const labels = { goldCurrency: "طلا وارز", cash: "نقدی", stock: "سهام" };
+    const data = this.props.data;
+    const pieChartData = data.map((item) => ({
+      name: labels[item.assetClass],
+      assetClass: item.assetClass,
+      value: item.overallValue,
+    }));
+    const COLORS = { goldCurrency: "gold", cash: "silver", stock: "green" };
+    return (
+      <div className="chartWrapper">
+        <PieChart width={500} height={500}>
+          <Pie
+            data={pieChartData}
+            dataKey="value"
+            nameKey="assetClass"
+            cx="50%"
+            cy={250}
+            label={renderCustomizedLabel}
+            labelLine={false}
+            outerRadius={200}
+            fill="#8884d8"
+          >
+            {pieChartData.map((entry, index) => (
+              <Cell key={index} fill={COLORS[entry.assetClass]} />
+            ))}
+          </Pie>
+          <Legend
+            formatter={(value, entry, index) => (
+              <span style={{ marginRight: "10px" }}>{value}</span>
+            )}
+            width={400}
+            wrapperStyle={{
+              top: 0,
+              right: 20,
+              backgroundColor: "#f5f5f5",
+              border: "1px solid #d5d5d5",
+              borderRadius: 3,
+              lineHeight: "40px",
+            }}
+          />
+        </PieChart>
+      </div>
+    );
+  }
+}
+
+export default QuotaChart;
