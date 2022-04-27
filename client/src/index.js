9cb3b94db5a9d6eb8a35017be2bbbb40c6009e08
MHMighani <mh.mgh2012@gmail.com>
2022-04-27 12:33:07 +0430

    redux initial setup

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import AssetsContextProvider from "./context/assetsContext";
+import reducers from "./reducers";
+import { configureStore } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.rtl.css";
 import "./fonts/b-nazanin-regular.ttf";
 import "./styles.scss";
+import { Provider } from "react-redux";
+
+const store = configureStore({ reducer: reducers, middleware: [thunk] });
 
 ReactDOM.render(
-  <AssetsContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </AssetsContextProvider>,
+  <Provider store={store}>
+    <AssetsContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AssetsContextProvider>
+  </Provider>,
   document.getElementById("root")
 );
 
---
+++ client/src/reducers/index.js
@@ -0,0 +1,3 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({ assets: () => "will return assets soon!" });
