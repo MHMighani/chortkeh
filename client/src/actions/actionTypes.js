c4f6cb6821aba1169be9baf6adcced613898309f
MHMighani <mh.mgh2012@gmail.com>
2022-04-30 21:45:33 +0430

    Refactor: action types

---
+++ client/src/actions/actionTypes.js
@@ -0,0 +1,13 @@
+// assets action types
+export const FETCH_ASSETS = "FETCH_ASSETS";
+export const ADD_ASSET = "ADD_ASSET";
+export const DELETE_ASSET_BY_ID = "DELETE_ASSET";
+export const DELETE_ASSET_BY_SUBCLASS = "DELETE_ASSET_BY_SUBCLASS";
+export const EDIT_ASSET = "EDIT_ASSET";
+
+// prices action types
+export const FETCH_PRICES = "FETCH_PRICES";
+
+// history action types
+export const FETCH_HISTORY = "FETCH_HISTORY";
+export const UPDATE_HISTORY = "UPDATE_HISTORY";
