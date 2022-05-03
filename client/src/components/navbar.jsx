59cc0b4ed49df8d353dcc727b20328afe8b42707
MHMighani <mh.mgh2012@gmail.com>
2022-05-03 20:38:47 +0430

    Feat: dark-mode context

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { Navbar as BootNavbar } from "react-bootstrap";
+import ThemeContext from "../context/themeContext";
 
 const Navbar = () => {
+  const themeContext = useContext(ThemeContext);
   return (
     <BootNavbar bg="light" expand="lg">
       <Container>
         <Link to="/" style={{ textDecoration: "none" }}>
           <BootNavbar.Brand>چرتکه</BootNavbar.Brand>
         </Link>
+        <button onClick={() => themeContext.toggleTheme()}>دارک مود</button>
       </Container>
     </BootNavbar>
   );
