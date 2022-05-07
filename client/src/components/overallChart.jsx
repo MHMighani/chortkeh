91e5c8da6cf3012384ac95bf4e3234b65bc5150e
MHMighani <mh.mgh2012@gmail.com>
2022-05-07 23:00:13 +0430

    Feat: add all categories in line chart

--- client/src/components/overallChart.jsx
+++ client/src/components/overallChart.jsx
@@ -7,17 +7,61 @@ import {
   CartesianGrid,
   Line,
 } from "recharts";
+import { useState } from "react";
 
 import getCommaSepNum from "../utils/getCommaSepNum";
 
 const OverallChart = ({ data }) => {
+  const [activeCategories, setActiveCategories] = useState(["overall"]);
+
+  const info = {
+    stock: { color: "#454B1B", label: "سهام", active: false },
+    cash: { color: "#C0C0C0", label: "نقدی", active: false },
+    goldCurrency: { color: "#FFD700", label: "طلا و ارز", active: false },
+    overall: { color: "#8884d8", label: "ارزش کل", active: true },
+  };
+
   const LineChartData = data.map((item) => {
     return {
       name: item.id,
       overall: item.overall,
       goldCurrency: item.goldCurrency,
+      stock: item.stock,
+      cash: item.cash,
     };
   });
+
+  const handleClick = (dataKey) => {
+    if (activeCategories.includes(dataKey)) {
+      setActiveCategories(activeCategories.filter((item) => item !== dataKey));
+    } else {
+      setActiveCategories([...activeCategories, dataKey]);
+    }
+  };
+
+  const renderCustomizedLegend = () => {
+    return (
+      <div className="customized-legend">
+        {Object.entries(info).map(([dataKey, value]) => {
+          const { label, color } = value;
+          const legendColor = activeCategories.includes(dataKey)
+            ? color
+            : "grey";
+          const style = {
+            display: "inline-block",
+            color: legendColor,
+            cursor: "pointer",
+            marginLeft: "1rem",
+          };
+          return (
+            <span style={style} onClick={() => handleClick(dataKey)}>
+              {label}
+            </span>
+          );
+        })}
+      </div>
+    );
+  };
 
   return (
     <div className="chartWrapper">
@@ -29,23 +73,27 @@ const OverallChart = ({ data }) => {
         data={LineChartData}
         margin={{ top: 5, right: 30, left: 30, bottom: 5 }}
       >
-        <Legend />
+        <Legend content={renderCustomizedLegend} />
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis style={{ fontSize: "14px" }} dataKey="name" />
         <YAxis
           style={{ fontSize: "14px" }}
           tickFormatter={(value) => getCommaSepNum(value)}
           domain={["dataMin- 10000000", "dataMax + 10000000"]}
-          dataKey="overall"
         />
         <Tooltip formatter={(value) => getCommaSepNum(value)} />
 
-        <Line
-          type="monotone"
-          dataKey="overall"
-          name="ارزش کل"
-          stroke="#8884d8"
-        />
+        {activeCategories.map((category) => {
+          const { label, color } = info[category];
+          return (
+            <Line
+              type="monotone"
+              dataKey={category}
+              name={label}
+              stroke={color}
+            />
+          );
+        })}
       </LineChart>
     </div>
   );
