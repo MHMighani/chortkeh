d1bf5af4e5a509d3842e16d27dda80938e18a817
MHMighani <mh.mgh2012@gmail.com>
2022-05-15 20:47:34 +0430

    Changes for client only version

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -10,27 +10,29 @@ import { deleteAsset } from "../services/assetsServices";
 
 import * as actions from "./actionTypes";
 
+const assets = [];
+
 export const fetchAssets = () => async (dispatch) => {
-  const response = await getAssets();
-  dispatch({ type: actions.FETCH_ASSETS, payload: response.data });
+  // const response = await getAssets();
+  dispatch({ type: actions.FETCH_ASSETS, payload: assets });
 };
 
 export const fetchPrices = () => async (dispatch) => {
-  const { data: goldCurrency } = await getPrices("goldcurrency");
-  const { data: stock } = await getPrices("stock");
+  // const { data: goldCurrency } = await getPrices("goldcurrency");
+  // const { data: stock } = await getPrices("stock");
 
   dispatch({
     type: actions.FETCH_PRICES,
-    payload: { goldCurrency, stock },
+    payload: { goldCurrency: [], stock: [] },
   });
 };
 
 export const fetchHistoryRecord = () => async (dispatch) => {
-  const { data: historyRecord } = await getHistoryRecord();
+  // const { data: historyRecord } = await getHistoryRecord();
 
   dispatch({
     type: actions.FETCH_HISTORY,
-    payload: historyRecord,
+    payload: [],
   });
 };
 
