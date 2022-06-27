554c0b9a461d11882e46df08fcfd63e5a4707d56
MHMighani <mh.mgh2012@gmail.com>
2022-06-27 22:30:41 +0430

    Improve selector performance

--- client/src/components/forms/selectForm.jsx
+++ client/src/components/forms/selectForm.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import Select from "react-select";
+import Select, { createFilter } from "react-select";
 
 const SelectForm = ({ error, label, id, ...props }) => {
   return (
     <div className="form-group">
       {label && <label htmlFor={id}>{label}</label>}
-      <Select id={id} {...props} />
+      <Select
+        id={id}
+        filterOption={createFilter({ ignoreAccents: false })}
+        {...props}
+      />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
