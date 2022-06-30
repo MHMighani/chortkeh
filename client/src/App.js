3d8b66ec6148e2327eb71c766916b7344b7ad6d3
MHMighani <mh.mgh2012@gmail.com>
2022-06-30 15:23:31 +0430

    Refactor: wrap app with Layout component

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,17 @@ import { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import GoldCurrencyForm from "./components/forms/goldCurrencyForm";
 import { ToastContainer } from "react-toastify";
-import NavbarT from "./components/layout/navbar";
 import StockForm from "./components/forms/stockForm";
 import CashForm from "./components/forms/cashForm";
 import AssetDetails from "./components/tables/assetsTable/assetDetails";
 import PortfolioDetails from "./components/portfolio/portfolioDetails";
 import Portfolio from "./components/portfolio/portfolio";
-import Footer from "./components/layout/footer";
 import { fetchAll } from "./actions";
 import { useDispatch } from "react-redux";
 import "react-toastify/dist/ReactToastify.css";
 import LoginForm from "./components/forms/loginForm";
 import SignupForm from "./components/forms/signupForm";
+import Layout from "./components/layout";
 
 function App() {
   const dispatch = useDispatch();
@@ -24,18 +23,18 @@ function App() {
 
   return (
     <div className="App">
-      <NavbarT />
-      <Switch>
-        <Route exact path="/" component={Portfolio} />
-        <Route path="/portfolio-details" component={PortfolioDetails} />
-        <Route path="/assets/details/" component={AssetDetails} />
-        <Route path="/add/goldcurrency/:id?" component={GoldCurrencyForm} />
-        <Route path="/add/stock/:id?" component={StockForm} />
-        <Route exact path="/add/cash/:id?" component={CashForm} />
-        <Route exact path="/login" component={LoginForm} />
-        <Route exact path="/signup" component={SignupForm} />
-      </Switch>
-      <Footer />
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Portfolio} />
+          <Route path="/portfolio-details" component={PortfolioDetails} />
+          <Route path="/assets/details/" component={AssetDetails} />
+          <Route path="/add/goldcurrency/:id?" component={GoldCurrencyForm} />
+          <Route path="/add/stock/:id?" component={StockForm} />
+          <Route exact path="/add/cash/:id?" component={CashForm} />
+          <Route exact path="/login" component={LoginForm} />
+          <Route exact path="/signup" component={SignupForm} />
+        </Switch>
+      </Layout>
       <ToastContainer position="top-center" hideProgressBar rtl icon={false} />
     </div>
   );
