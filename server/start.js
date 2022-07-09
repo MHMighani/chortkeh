c165c220ac66fc5d4c635c04aac1e78762a0d834
MHMighani <mh.mgh2012@gmail.com>
2022-07-09 16:41:41 +0430

    Add auth guard rules

--- server/start.js
+++ server/start.js
@@ -9,11 +9,20 @@ const PORT = runningPort;
 const app = jsonServer.create();
 const router = jsonServer.router(databasePath);
 
+// auth guard rules
+const rules = auth.rewriter({
+  assets: 600,
+  history: 600,
+  GoldCurrency: 660,
+  stock: 660,
+});
+
 const middlewares = jsonServer.defaults();
 
 // binding router db to the app for json-server-auth usage
 app.db = router.db;
 
+app.use(rules);
 app.use(middlewares);
 app.use(auth);
 app.use(router);
