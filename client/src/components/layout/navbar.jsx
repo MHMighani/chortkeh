07402e9f3f222b0f457498fe5466882251e5438c
MHMighani <mh.mgh2012@gmail.com>
2022-07-15 19:28:48 +0430

    Fix: navbar user menu hide user menu when user is not logged in

--- client/src/components/layout/navbar.jsx
+++ client/src/components/layout/navbar.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Menu from "../common/menu";
+import useAuth from "../../hooks/useAuth";
 import { Navbar as BootNavbar } from "react-bootstrap";
 
 const Navbar = () => {
+  const { token } = useAuth();
+
+  const userSection = token ? <Menu /> : null;
+
   return (
     <BootNavbar height="100">
       <Container>
         <Link to="/" style={{ textDecoration: "none" }}>
           <BootNavbar.Brand>چرتکه</BootNavbar.Brand>
         </Link>
-        <Menu />
+        {userSection}
       </Container>
     </BootNavbar>
   );
