d6b7d1b2a91bb72b29701311ecf00983f437fa1b
MHMighani <mh.mgh2012@gmail.com>
2022-07-31 10:10:17 +0430

    Feat: log buttons in navbar

--- client/src/components/layout/navbar.jsx
+++ client/src/components/layout/navbar.jsx
@@ -8,7 +8,17 @@ import { Navbar as BootNavbar } from "react-bootstrap";
 const Navbar = () => {
   const { token } = useAuth();
 
-  const userSection = token ? <Menu /> : null;
+  const logButtons = (
+    <div className="log-buttons">
+      <Link className="btn btn-light" to="/login">
+        ورود
+      </Link>
+      <Link className="btn btn-light" to="/signup">
+        ثبت‌نام
+      </Link>
+    </div>
+  );
+  const userSection = token ? <Menu /> : logButtons;
 
   return (
     <BootNavbar height="100">
