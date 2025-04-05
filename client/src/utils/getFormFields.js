d0c43801961196e2135db35608584ed9416e3d9b
m.mighani <mhmighani.dev@gmail.com>
2025-04-05 15:33:53 +0330

    chore: fresh server setup

--- client/src/utils/getFormFields.js
+++
@@ -1,7 +0,0 @@
-// extract form fields from form-state
-export default function getFormFields(formState) {
-  return Object.values(formState).reduce((prev, current) => {
-    if (current.name === "formName") return null;
-    return { ...prev, [current.name]: current.value };
-  }, {});
-}
--- client/src/utils/getPercentChange.js
+++
@@ -1,7 +0,0 @@
-function getPercentChange(firstValue, secondValue, decimalPoint = 2) {
-  return Number(
-    (((secondValue - firstValue) / firstValue) * 100).toFixed(decimalPoint)
-  );
-}
-
-export default getPercentChange;
